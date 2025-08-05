b526825dd710abac6f306e1b558293f122c5e344
DevrajBhore <bhoredevraj896@gmail.com>
2025-08-05 21:20:52 +0530

    updated axiosConfig.js

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -2,16 +2,8 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_URL_API || "https://nomadgym.onrender.com/api/v1/",
+  baseURL: import.meta.env.VITE_URL_API || "https://nomadgym.onrender.com/api/v1",
   withCredentials: true,
 })
-
-// API.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("token")
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`
-//   }
-//   return config"""
-// })
 
 export default API

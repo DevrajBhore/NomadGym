b455e4e4be0f18aa3f27848d1c69b86667250f6a
Varun Bhore <varunbhore@Varuns-MacBook-Air.local>
2025-08-05 17:34:41 +0530

    updated axiosConfig.js

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_URL_API || "http://localhost:2000/api/",
+  baseURL: import.meta.env.VITE_URL_API,
   withCredentials: true,
 })
 
